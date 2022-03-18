42fc0cd23c1d1416977dc7b473264c06021a0b23
Jeffrey-Meijer <jeffreymeijer11@gmail.com>
2022-03-18 16:59:44 +0100

    Added rest endpoint for elementor footers

---
+++ Plugin/ExposeElementor.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package ExposeElementor
+ */
+/*
+ 
+Plugin Name: Expose Elementor REST API
+Plugin URI: https://hq-online.nl
+Description: Exposes the Elementor content to the REST API for further use
+Version: 1.0
+Author: HQ Online
+Author URI: https://hq-online.nl/
+License: GPLv2 or later
+Text Domain: exposeelementor
+*/
+
+
+add_action("rest_api_init", "expose_elementor_content");
+
+function expose_elementor_content() {
+    register_rest_route(
+        "hq/v1", "/elementor/footer",
+        [
+            "methods" => "GET",
+            "callback" => function(\WP_REST_Request $req) {
+                $contentElementor = "{}";
+                $args = array (
+                    "post_type" => "elementor_library",
+                    "posts_per_page" => -1
+                );
+
+                if (class_exists("\\Elementor\\Plugin")) {
+                    $post_id = $req->get_param("id");
+                    
+                    $pluginElementor = \Elementor\Plugin::instance();
+                    $the_query = new WP_Query($args);
+                    
+                    if($the_query->have_posts()): 
+                        while($the_query->have_posts()) : $the_query->the_post();
+                            $post_meta = get_post_meta(get_the_ID());
+                            if(in_array("footer",$post_meta["_elementor_template_type"])) {
+                                return $post_meta;
+                            }
+                        endwhile;
+                        wp_reset_query();
+                    endif;
+
+                }
+
+                return $contentElementor;
+            }
+        ]
+    );
+    register_rest_route(
+        "hq/v1", "/elementor/footer",
+        [
+            "methods" => "POST",
+            "callback" => function(\WP_REST_Request $req) {
+                $id = $req["id"];
+                $data = $req["data"];
+                // update_post_meta($id, "_elementor_data", $data);
+            }
+        ]
+    )
+}
+
+?>
---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import bs4 as bs
+
+def get_footer():
+    r = requests.get('https://tegelsensanitairbeilen.nl/wp-json/hq/v1/elementor/footer')
+    # data = json.dumps(r.json()['_elementor_data'][0]).encode('utf-8').decode('unicode-escape') # Elementor footer data
+    data = json.loads(r.json()['_elementor_data'][0])
+    for element in data:
+        # if element["editor"]
+        if element["elements"]["settings"]["editor"]:
+            print(element["elements"]["settings"]["editor"])        
+        else:
+            print(element["elements"]["elements"]["settings"]["editor"])
+        # print(element["elements"])
+        # break
+    # soup = bs.BeautifulSoup(data)
+
+    # print(data)
+
+def main():
+    get_footer()
+
+
+if __name__ == "__main__":
+    main()
