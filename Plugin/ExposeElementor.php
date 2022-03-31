bd4013a432886df28aa61c59000c2a4dee6a2f9d
Jeffrey-Meijer <jeffreymeijer11@gmail.com>
2022-03-31 12:42:16 +0200

    W.I.P recursive footer searching, footer edit code

--- Plugin/ExposeElementor.php
+++ Plugin/ExposeElementor.php
@@ -56,12 +56,36 @@ function expose_elementor_content() {
         [
             "methods" => "POST",
             "callback" => function(\WP_REST_Request $req) {
-                $id = $req["id"];
-                $data = $req["data"];
-                // update_post_meta($id, "_elementor_data", $data);
+                $args = array(
+                    "post_type" => "elementor_library",
+                    "posts_per_page" => -1
+                );
+                if (class_exists("\\Elementor\\Plugin")) {
+                    $post_id = $req->get_param("id");
+                    $parameters = $req->get_params();
+
+                    $pluginElementor = \Elementor\Plugin::instance();
+                    $the_query = new WP_Query($args);
+
+                    if ($the_query->have_posts()) :
+                        while ($the_query->have_posts()) : $the_query->the_post();
+                            $post_meta = get_post_meta(get_the_ID());
+                            if (in_array("footer", $post_meta["_elementor_template_type"])) {
+                                $new_data = $parameters["_elementor_data"];
+                                // update_post_meta(get_the_ID(), "_elementor_data", $new_data);
+                                // return $new_data;
+                                // return $post_id;
+                            }
+                        endwhile;
+                        wp_reset_query();
+                    endif;
+                }
+                // $id = $req->get_param("id");
+                // $data = $req->get_param("data");
+                // return $data;
             }
         ]
-    )
+    );
 }
 
 ?>
--- main.py
+++ main.py
@@ -1,27 +1,77 @@
+from dataclasses import replace
 import requests
 import json
 import bs4 as bs
 
-def get_footer():
-    r = requests.get('http://localhost/linkscript/wp-json/hq/v1/elementor/footer')
-    data = json.loads(r.json()['_elementor_data'][0])
+def replace_footer(data=None):
+    # if r == None:
+    r = requests.get("http://localhost/linkscript/wp-json/hq/v1/elementor/footer")
+    if data is None:
+        data = json.loads(r.json()["_elementor_data"][0])
     for element in data:
-        for el in element["elements"]:
-            if el["elements"]:
-                for l in el["elements"]:
-                    if l["settings"]["editor"]:
-                        toEdit = l["settings"]["editor"]
+        if element.get('settings'):
+            if element['settings'].get('editor'):
+                print('exists')
+                print(element["settings"]["editor"])
+                toEdit = element["settings"]["editor"]
+                toEdit = toEdit.replace("https://webdesignhq.nl/", "https://hqonline.nl/")
+                toEdit = toEdit.replace("Webdesignhq", "HQ Online")
+                # return element['settings']['editor']
+                # return element
+            else:
+                return replace_footer(element['elements'])
+        else:
+            return replace_footer(element['elements'])
 
-    
-    print(toEdit)
+    return json.dumps([element])
+    # print(data[0])
+    # if data[0].get("editor"):
+        # print("exists")
+        # return replace_footer(data, r)
+    # else:
+        # for element in data:
+        #     for el in element["elements"]:
+        #         try:
+        #             if el["settings"]["editor"]:
+        #                 toEdit = el["settings"]["editor"]
+        #                 toEdit = toEdit.replace("https://webdesignhq.nl/", "https://hqonline.nl/") # Change URL
+        #                 toEdit = toEdit.replace("Webdesignhq", "HQ Online")
+        #                 el["settings"]["editor"] = toEdit
+        #                 return [element]
+        #         except KeyError:
+        #             return replace_footer(el, r)
+                # else:
+                    # return replace_footer(el, r)
+            # if el["elements"]:
+            #     for l in el["elements"]:
+            #         if l["settings"]["editor"]:
+            #             toEdit = l["settings"]["editor"]
+            #             toEdit = toEdit.replace("https://webdesignhq.nl/", "https://hqonline.nl/") # Change URL
+            #             toEdit = toEdit.replace("Webdesignhq", "HQ Online")
+            #             l["settings"]["editor"] = toEdit
+                        
+        # return json.dumps([element])
 
-    toEdit = toEdit.replace("https://webdesignhq.nl/", "https://hqonline.nl/") # Change URL
-    toEdit = toEdit.replace("Webdesignhq", "HQ Online")
-    print(toEdit)
+
+
+def send_data(new_footer):
+    print(new_footer.replace("\\", "\\\\"))
+    obj = {
+        "_elementor_data": new_footer.replace("\\", "\\\\")
+    }
+    # print(json.dumps(obj))
+    # r = requests.post("http://localhost/linkscript/wp-json/hq/v1/elementor/footer", data=obj)
+    status = r.status_code
+    if status == 200:
+        # print("data sent")
+        print(r.text)
+    else:
+        print(status)
 
 
 def main():
-    get_footer()
+    new_footer = replace_footer()
+    send_data(new_footer)
 
 
 if __name__ == "__main__":
